22773001e3a4bc08ceeedde63e5b11bb13b62e57
ChrisLiang7 <zliang15@calpoly.edu>
2023-01-06 16:38:49 -0800

    feat: Create New Note

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,53 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import { Routes, Route, Navigate } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { NewNote } from "./NewNote";
+import { useState } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: String[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
 
 function App() {
-  return( 
-  <Routes>
-    <Route path="*" element={<Navigate to="/" />} />
-    <Route path="/" element={<h1>Home</h1>} />
-    <Route path="about" element={<h1>About</h1>} />
-  </Routes>
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
 
-  )
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="about" element={<h1>About</h1>} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
